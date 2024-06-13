18eba05dd7965c03b2e7f685b3461cecd0acd97f
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-06-12 23:26:14 -0700

    worked on home page css

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -4,25 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = ({ showAuthButtons }) => {
   return (
-    <Navbar bg="light" className="w-100">
+    <div>
+    <Navbar className="w-100" style={{backgroundColor: "goldenrod"}}>
       <Container>
         <Row className="w-100">
           <Col className="d-flex align-items-center">
-            <Button variant="outline-info" href="/" className="me-3">Home</Button>
+            <Button variant= "outline-light" href="/" className="me-3" style ={{borderColor: "black", color: "#4169e1", fontWeight: "bold", color: "black"}}>Home</Button>
           </Col>
           <Col className="d-flex justify-content-center">
-            <Navbar.Brand>Bonded by Adventure</Navbar.Brand>
+            <Navbar.Brand style={{fontSize: "45px", fontWeight: "bold", fontFamily: "calibri", color: "black", textShadow: "2px 2px 10px yellow"}}>Bonded by Adventure</Navbar.Brand>
           </Col>
           <Col className="d-flex justify-content-end">
-              <div>
-                <Button variant="outline-warning" href="/signUp" className="me-2">Sign Up</Button>
-                <Button variant="warning" href="/signIn">Sign In</Button>
+              <div className="d-flex align-items-center">
+                <Button variant = "outline-light" href="/signUp" className="me-2" style={{borderColor: "#ffd700", color: "#4169e1", fontWeight: "bold", color: "black"}}>Sign Up</Button>
+                <Button variant = "outline-light" href="/signUp" className="me-2" style={{borderColor: "#ffd700", color: "#4169e1", fontWeight: "bold", color: "black"}}>Sign In</Button>
               </div>
             
           </Col>
         </Row>
       </Container>
     </Navbar>
+    </div>
   );
 };
 
