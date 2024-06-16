fb3ca86a04b3cf6aa2551908d9d9d1d25fbf69e0
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-06-15 17:42:35 -0700

    completed signup form to database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,9 +12,17 @@ exports.getAllUsers = async (req, res, next) => {
 
 exports.createUser = async (req, res, next) => {
   try {
-    const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+    const { firstName, lastName, familyName, email, password } = req.body;
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      familyName,
+      email,
+      password
+      // Add other fields as needed
+    });
+    res.status(201).json(newUser); // Respond with the created user object
   } catch (error) {
-    next(error);
+    next(error); // Pass error to the error handler middleware
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const { sequelize } = require('./models'); // Import Sequelize instance from models
+const cors = require('cors');
+const errorHandler = require('./middleware/errorHandler');
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
 // Error handling middleware
-const errorHandler = require('./middleware/errorHandler');
 app.use(errorHandler);
 
 // Test the database connection function
